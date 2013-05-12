2fe07c4961d3b884ad0031495441b36a062d6952
Glenn Nagel <glenn@mercury-wireless.com>
2013-05-12 14:17:06 -0400

    Updated calculations for IV spec

--- spec/greeks/calculations/iv_spec.rb
+++ spec/greeks/calculations/iv_spec.rb
@@ -5,68 +5,52 @@ describe "Math::GreekCalculations::iv" do
   include Math::GreekCalculations
   include Math::GreekCalculationHelpers
 
+  let(:stock_price) { 10.00 }
+  let(:stock_dividend_rate) { 0.0 }
+  let(:stock_dividend_rate_f) { 0.0 }
+
+  let(:option_price) { 1.00 } 
+  let(:option_expires_in_days) { 364.0 }
+  let(:option_expires_pct_year) { 1.0 }
 
   let(:federal_reserve_interest_rate) { 0.0 }
-  let(:federal_reserve_interest_rate_f) { federal_reserve_interest_rate / 100.0 }
-  let(:stock_dividend_rate) { 0.0 }
-  let(:stock_dividend_rate_f) { stock_dividend_rate / 100 }
-  let(:stock_price) { 1558.86 }
-  let(:option_expires_in_days) { 2.0 }
-  let(:option_expires_pct_year) { (option_expires_in_days + 1.0) / 365.0 }
+  let(:federal_reserve_interest_rate_f) { 0.00 }
 
   describe "call options" do
     let(:option_type) { :call }
-
-    context "should calculate the iv" do
-      let(:option_strike) {  800.00 }
-      let(:option_price) { 751.50 } 
-      it { var_iv().should be_nil }
+    
+    context "exactly at the money" do
+      let(:option_strike) { 10.00 }
+      it { var_iv().should === 0.25089263455361754 }
     end
-
-    context "should calculate the iv" do
-      let(:option_strike) { 1555.00 }
-      let(:option_price) {   6.00 } 
-      it { var_iv().round(4).should === 0.0668 }
+    
+    context "out of the money" do
+      let(:option_strike) { 15.00 }
+      it { var_iv().should === 0.5819996182323802 }
     end
-
-    context "should calculate the iv" do
-      let(:option_strike) { 1560.00 }
-      let(:option_price) {   3.70 } 
-      it { var_iv().round(4).should === 0.0753 }
-    end
-
-    context "should calculate the iv" do
-      let(:option_strike) { 1565.00 }
-      let(:option_price) {   2.00 } 
-      it { var_iv().round(4).should === 0.0780 }
+    
+    context "in the money" do
+      let(:option_strike) { 5.00 }
+      it { var_iv().should === nil }
     end
   end
   
   describe "put options" do
     let(:option_type) { :put }
 
-    context "should calculate the iv" do
-      let(:option_strike) {  800.00 }
-      let(:option_price) { 751.50 } 
-      it { var_iv().should be_nil }
+    context "exactly at the money" do
+      let(:option_strike) { 10.00 }
+      it { var_iv().should === 0.25089263455361754 }
     end
-
-    context "should calculate the iv" do
-      let(:option_strike) { 1555.00 }
-      let(:option_price) {   8.00 } 
-      it { var_iv().round(4).should === 0.1742 }
+    
+    context "out of the money" do
+      let(:option_strike) { 15.00 }
+      it { var_iv().should === nil }
     end
-
-    context "should calculate the iv" do
-      let(:option_strike) { 1560.00 }
-      let(:option_price) {   10.40 } 
-      it { var_iv().round(4).should === 0.1741 }
-    end
-
-    context "should calculate the iv" do
-      let(:option_strike) { 1565.00 }
-      let(:option_price) {   13.60 } 
-      it { var_iv().round(4).should === 0.1812 }
+    
+    context "in the money" do
+      let(:option_strike) { 5.00 }
+      it { var_iv().should === 1.0245859277769118 }
     end
   end
 end
