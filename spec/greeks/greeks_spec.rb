c807a843c8d72302c209c043d87bfe2ff133116f
Glenn Nagel <glenn@mercury-wireless.com>
2013-05-29 21:24:28 -0400

    Working on spec issues w/test data

--- spec/greeks/greeks_spec.rb
+++ spec/greeks/greeks_spec.rb
@@ -113,4 +113,107 @@ describe Math::Greeks::Calculator do
     calculator.iv.should be_nil
     calculator.to_hash[:iv].should be_nil
   }
+  
+  context "bulk testing" do
+    let(:stock_price) { 3.98 }
+    let(:dividends) { 0.00 }
+
+    context "23 days" do
+      let(:days){ 23 }
+      let(:fed) { 0.04 }
+      
+      context "call" do
+        let(:hash) {
+          Math::Greeks::Calculator.new(
+            :stock_price                   => stock_price,
+            :stock_dividend_rate           => dividends,
+            :federal_reserve_interest_rate => fed,
+            :option_expires_in_days        => days, 
+            :option_type                   => :call, 
+            :option_strike                 => 3.50,
+            :option_price                  => 0.54).to_hash
+        }
+   
+        it { hash[:iv].round(1).should === 58.5.round(1) }
+        it { hash[:delta].round(1).should === 6.1.round(1) }
+        it { hash[:gamma].round(1).should === 2.1.round(1) }
+        it { hash[:vega].round(1).should === 0.29.round(1) }
+        it { hash[:rho].round(1).should === 0.33.round(1) }
+        it { hash[:theta].round(1).should === -0.60.round(1) }
+        it { hash[:delta_vs_theta].round(1).should === -10.20.round(1) }
+        it { hash[:break_even].round(1).should === 43.07.round(1) }
+      end
+      
+      context "put" do
+        let(:hash) {
+          Math::Greeks::Calculator.new(
+            :stock_price                   => stock_price,
+            :stock_dividend_rate           => dividends,
+            :federal_reserve_interest_rate => fed,
+            :option_expires_in_days        => days, 
+            :option_type                   => :put, 
+            :option_strike                 => 3.50,
+            :option_price                  => 0.07).to_hash
+        }
+   
+        it { hash[:iv].round(1).should === 62.2.round(1) }
+        it { hash[:delta].round(1).should === -10.7.round(1) }
+        it { hash[:gamma].round(1).should === -9.0.round(1) }
+        it { hash[:vega].round(1).should === 2.44.round(1) }
+        it { hash[:rho].round(1).should === -0.77.round(1) }
+        it { hash[:theta].round(1).should === -5.08.round(1) }
+        it { hash[:delta_vs_theta].round(1).should === 2.10.round(1) }
+        it { hash[:break_even].round(1).should === 19.66.round(1) }
+      end
+    end
+
+    context "597 days" do
+      let(:days){ 597 }
+      let(:fed) { 0.24 }
+      
+      context "call" do
+        let(:hash) {
+          Math::Greeks::Calculator.new(
+            :stock_price                   => stock_price,
+            :stock_dividend_rate           => dividends,
+            :federal_reserve_interest_rate => fed,
+            :option_expires_in_days        => days, 
+            :option_type                   => :call, 
+            :option_strike                 => 3.50,
+            :option_price                  => 1.35).to_hash
+        }
+   
+        it { hash[:iv].round(1).should === 58.2.round(1) }
+        it { hash[:delta].round(1).should === 2.1.round(1) }
+        it { hash[:gamma].round(1).should === 0.7.round(1) }
+        it { hash[:vega].round(1).should === 0.75.round(1) }
+        it { hash[:rho].round(1).should === 1.79.round(1) }
+        it { hash[:theta].round(1).should === -0.06.round(1) }
+        it { hash[:delta_vs_theta].round(1).should === -32.81.round(1) }
+        it { hash[:break_even].round(1).should === 26.35.round(1) }
+      end
+      
+      context "put" do
+        let(:hash) {
+          Math::Greeks::Calculator.new(
+            :stock_price                   => stock_price,
+            :stock_dividend_rate           => dividends,
+            :federal_reserve_interest_rate => fed,
+            :option_expires_in_days        => days, 
+            :option_type                   => :put, 
+            :option_strike                 => 3.50,
+            :option_price                  => 0.92).to_hash
+        }
+   
+        it { hash[:iv].round(1).should === 61.8.round(1) }
+        it { hash[:delta].round(1).should === -1.2.round(1) }
+        it { hash[:gamma].round(1).should === -1.5.round(1) }
+        it { hash[:vega].round(1).should === 1.17.round(1) }
+        it { hash[:rho].round(1).should === -3.66.round(1) }
+        it { hash[:theta].round(1).should === -0.10.round(1) }
+        it { hash[:delta_vs_theta].round(1).should === 12.93.round(1) }
+        it { hash[:break_even].round(1).should === 43.74.round(1) }
+      end
+    end
+  end
 end
